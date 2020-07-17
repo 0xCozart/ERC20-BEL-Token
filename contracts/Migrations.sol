973ee35d1de8e36030846598642447a409cb8ff3
alan <alanvega002@gmail.com>
2020-07-17 17:07:28 +0000

    Slimed down contracts

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.4.21 <0.7.0;
 
 contract Migrations {
     address public owner;
+    // A function with the signature `last_completed_migration()`, returning a uint, is required
     uint256 public last_completed_migration;
 
     constructor() public {
