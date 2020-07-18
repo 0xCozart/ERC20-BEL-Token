9acdd9eb24b2d8ef881d6c55700b873c34d63dce
alan <alanvega002@gmail.com>
2020-07-18 22:52:14 +0000

    Changed initia token supply

--- contracts/BelToken.sol
+++ contracts/BelToken.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.6.9;
 import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract BelToken is ERC20 {
-    uint256 public INIT_SUPPLY = 42000000000000000000;
+    uint256 public INIT_SUPPLY = 700000;
 
     constructor() public ERC20("Bel", "BEL") {
         _mint(msg.sender, INIT_SUPPLY);
